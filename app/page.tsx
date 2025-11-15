9ab4dd425ff68996887c805e3ed7461c50785620
Florian Knauf <knauf@vss.uni-hannover.de>
2025-11-15 16:06:23 +0100

    make layout responsive

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
           }
         </ActionButton>
       </Flex>
-      <Flex direction="row" gap="size-100" justifyContent="center">
+      <Flex direction="row" gap="size-100" justifyContent="center" wrap={true}>
         {
           selectedDisplayStreams.map((stream, ix) =>
             <PreviewCard
