00a80ce9454984b927773ada609519b58b3da6bf
Florian Knauf <knauf@vss.uni-hannover.de>
2025-11-16 14:44:06 +0100

    docker support

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,19 @@ import type { NextConfig } from "next";
 import { glob } from 'glob';
 
 module.exports = {
-  transpilePackages: [
-    '@adobe/react-spectrum',
-    '@react-spectrum/*',
-    '@spectrum-icons/*'
-  ].flatMap((spec) => glob.sync(`${spec}`, { cwd: 'node_modules/' }))
 };
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  transpilePackages: [
+    '@adobe/react-spectrum',
+    '@react-spectrum/*',
+    '@spectrum-icons/*'
+  ].flatMap((spec) => glob.sync(`${spec}`, { cwd: 'node_modules/' })),
+  output: "standalone"
 };
+
+module.exports = nextConfig
 
 export default nextConfig;
