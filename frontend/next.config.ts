4b494ad409bbae393dde7912e08ef80924c4e8ed
Florian Knauf <knauf@vss.uni-hannover.de>
2025-11-22 16:17:55 +0100

    fix typos in content security policy

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -33,7 +33,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; imgr-src 'self' blob: data:; font-src 'self'; object-src 'none'; base-uri: 'self'; form-action: 'self'; frame-ancestors: 'none'; upgrade-insecure-requests;"
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data:; font-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests;"
           }
         ]
       }
