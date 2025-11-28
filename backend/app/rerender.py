c896031bde54a951bf31429fdfb40f832cc7fa19
Florian Knauf <knauf@vss.uni-hannover.de>
2025-11-28 20:52:21 +0100

    Make pylint happy

--- backend/app/rerender.py
+++ backend/app/rerender.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
+"""
+Program to rerender a recording from chunks on the ise-recorder backend server
+"""
+
 from argparse import ArgumentParser
 import logging
 from pathlib import Path
-import sys
 
 from ise_record.postprocess import postprocess_recording
 
 def main():
+    """
+    Main function. Parses command line, calls postprocessing and prints results.
+    """
+
     parser = ArgumentParser(
         prog="ise-rerender",
         description="Manually (re)execute postprocessing for a ise-recorder recording"
