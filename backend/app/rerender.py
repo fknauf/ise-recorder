de3c783f1c0ede2dd89f92e9e45413f8fbeb75f1
Florian Knauf <knauf@vss.uni-hannover.de>
2025-11-28 21:06:27 +0100

    cosmetic cleanup in backend rerender script

--- backend/app/rerender.py
+++ backend/app/rerender.py
@@ -19,16 +19,12 @@ def main():
         prog="ise-rerender",
         description="Manually (re)execute postprocessing for a ise-recorder recording"
     )
-
-    parser.add_argument('recording_directory')
+    parser.add_argument('recording_directory', type=Path)
     parser.add_argument('-l', '--log-level', default="INFO")
-
     args = parser.parse_args()
 
     logging.basicConfig(level=args.log_level)
-
-    result = postprocess_recording(Path(args.recording_directory))
-
+    result = postprocess_recording(args.recording_directory)
     print(f"Result: {result.reason.name}, output = {result.output_file}")
 
 if __name__ == "__main__":
