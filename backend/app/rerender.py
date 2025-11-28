afe0d479ab64f56e2ea81281469fe5bf452b2c20
Florian Knauf <knauf@vss.uni-hannover.de>
2025-11-28 20:00:53 +0100

    Improve backend rerender script

--- backend/app/rerender.py
+++ backend/app/rerender.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
 import logging
 from pathlib import Path
 import sys
@@ -5,8 +8,21 @@ import sys
 from ise_record.postprocess import postprocess_recording
 
 def main():
-    logging.basicConfig(level=logging.INFO)
-    print(postprocess_recording(Path(sys.argv[1])))
+    parser = ArgumentParser(
+        prog="ise-rerender",
+        description="Manually (re)execute postprocessing for a ise-recorder recording"
+    )
+
+    parser.add_argument('recording_directory')
+    parser.add_argument('-l', '--log-level', default="INFO")
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=args.log_level)
+
+    result = postprocess_recording(Path(args.recording_directory))
+
+    print(f"Result: {result.reason.name}, output = {result.output_file}")
 
 if __name__ == "__main__":
     main()
