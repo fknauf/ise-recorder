3de3e681a13a31b1135f79c60d9164df5b1835c6
Florian Knauf <knauf@vss.uni-hannover.de>
2025-12-20 22:07:18 +0100

    postprocessing tests

--- frontend/__tests__/util/serverStorage.test.ts
+++ frontend/__tests__/util/serverStorage.test.ts
@@ -58,7 +58,7 @@ test("sending chunk to flaky server", async () => {
       return Response.error();
     });
 
-  await sendChunkToServer(apiUrl, chunk, "FOO", "stream.webm", 42);
+  await sendChunkToServer(apiUrl, chunk, "FOO", "stream.webm", 42, { retries: 10, intervalMillis: 50 });
 
   expect(fetchRequests.length).toBe(2);
 
@@ -94,7 +94,7 @@ test("sending chunk to broken server", { timeout: 30000 }, async () => {
   expect(vi.mocked(showError)).toHaveBeenCalled();
 
   expect(fetchRequests.length).toBe(4);
-  expect(after.getTime() - before.getTime()).toBeGreaterThan(150);
+  expect(after.getTime() - before.getTime()).toBeGreaterThan(149);
   expect(after.getTime() - before.getTime()).toBeLessThan(200);
 
   for(const req of fetchRequests) {
@@ -108,5 +108,85 @@ test("sending chunk to broken server", { timeout: 30000 }, async () => {
     expect(requestBody.get("track")).toStrictEqual("stream.webm");
     expect(requestBody.get("index")).toStrictEqual("42");
     expect(await (requestBody.get("chunk") as File).text()).toStrictEqual(await chunk.text());
+  }
+});
+
+test("schedule postprocessing", async () => {
+  const apiUrl = "http://record.example.com";
+
+  let fetchRequest: FetchRequest = { url: "" };
+
+  window.fetch = vi.fn()
+    .mockImplementation(async (url: string | URL | Request, data?: RequestInit): Promise<Response> => {
+      fetchRequest = { url, data };
+      return Response.json("");
+    });
+
+  await schedulePostprocessing(apiUrl, "FOO", "lecturer@example.com");
+
+  expect(fetchRequest.url).toBe(`${apiUrl}/api/jobs`);
+  expect(fetchRequest.data?.method).toBe("POST");
+  expect(fetchRequest.data?.headers).toStrictEqual({ "Content-Type": "application/json" });
+
+  const requestBody = JSON.parse(fetchRequest.data?.body as string);
+  expect(requestBody).toStrictEqual({ recording: "FOO", recipient: "lecturer@example.com" });
+});
+
+test("schedule postprocessing to flaky server", async () => {
+  const apiUrl = "http://record.example.com";
+
+  const fetchRequests: FetchRequest[] = [];
+
+  window.fetch = vi.fn()
+    .mockImplementation(async (url: string | URL | Request, data?: RequestInit): Promise<Response> => {
+      fetchRequests.push({ url, data });
+      return Response.json("");
+    })
+    .mockImplementationOnce(async (url: string | URL | Request, data?: RequestInit): Promise<Response> => {
+      fetchRequests.push({ url, data });
+      return Response.json("", { status: 503 });
+    });
+
+  await schedulePostprocessing(apiUrl, "FOO", "lecturer@example.com", { retries: 5, intervalMillis: 50 });
+
+  expect(fetchRequests.length).toBe(2);
+
+  for(const req of fetchRequests) {
+    expect(req.url).toBe(`${apiUrl}/api/jobs`);
+    expect(req.data?.method).toBe("POST");
+    expect(req.data?.headers).toStrictEqual({ "Content-Type": "application/json" });
+
+    const requestBody = JSON.parse(req.data?.body as string);
+    expect(requestBody).toStrictEqual({ recording: "FOO", recipient: "lecturer@example.com" });
+  }
+});
+
+test("schedule postprocessing to broken server", async () => {
+  const apiUrl = "http://record.example.com";
+
+  const fetchRequests: FetchRequest[] = [];
+
+  window.fetch = vi.fn()
+    .mockImplementation(async (url: string | URL | Request, data?: RequestInit): Promise<Response> => {
+      fetchRequests.push({ url, data });
+      return Response.error();
+    });
+
+  const before = new Date();
+  await schedulePostprocessing(apiUrl, "FOO", "lecturer@example.com", { retries: 3, intervalMillis: 50 });
+  const after = new Date();
+
+  expect(fetchRequests.length).toBe(4);
+  expect(after.getTime() - before.getTime()).toBeGreaterThan(149);
+  expect(after.getTime() - before.getTime()).toBeLessThan(200);
+  expect(vi.mocked(showError)).toHaveBeenCalled();
+
+  for(const req of fetchRequests) {
+    expect(req.url).toBe(`${apiUrl}/api/jobs`);
+    expect(req.data?.method).toBe("POST");
+    expect(req.data?.headers).toStrictEqual({ "Content-Type": "application/json" });
+
+    const requestBody = JSON.parse(req.data?.body as string);
+    expect(requestBody).toStrictEqual({ recording: "FOO", recipient: "lecturer@example.com" });
   }
 });
