42208deb3ba5fd00a5bc190ac7b6406512d4d5da
Florian Knauf <knauf@vss.uni-hannover.de>
2025-12-21 11:16:30 +0100

    remove dependency on media-mock package in video preview tests

--- frontend/__tests__/components/VideoPreview.test.tsx
+++ frontend/__tests__/components/VideoPreview.test.tsx
@@ -1,61 +1,71 @@
 import { expect, test, vi } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import { VideoPreview } from "@/app/lib/components/VideoPreview";
-import { MediaMock, devices } from "@eatsjobs/media-mock";
 import userEvent from "@testing-library/user-event";
 import { defaultTheme, Provider } from "@adobe/react-spectrum";
+import { useEffect, useState } from "react";
 
 test("VideoPreview displays video track", async () => {
   const width = 640;
   const height = 480;
   const srcImageUrl = "__tests__/assets/lorempicsum-560-640x480.png";
 
-  MediaMock.mock(devices["Mac Desktop"]);
-  await MediaMock.setMediaURL(srcImageUrl);
+  let setPreviewTrack: (track: MediaStreamTrack) => void = () => {};
 
-  const stream = await navigator.mediaDevices.getUserMedia({
-    video: {
-      width: width,
-      height: height
-    },
-    audio: false
-  });
-  const track = stream.getVideoTracks().at(0) as MediaStreamTrack;
+  const TestComponent = () => {
+    const [ track, setTrack ] = useState<MediaStreamTrack>();
 
-  // canary in case someone ever fiddles with the playwright configuration. media-mock
-  // switches width and height around when it believes the window is in portrait orientation
-  expect(track.getSettings().height).toBe(height);
-  expect(track.getSettings().width).toBe(width);
+    useEffect(() => {
+      setPreviewTrack = setTrack;
+    }, []);
 
-  render(
-    <Provider theme={defaultTheme}>
-      <VideoPreview
-        track={track}
-        width={width}
-        height={height}
-        isMainDisplay={true}
-        isOverlay={false}
-        switchesDisabled={false}
-        onToggleMainDisplay={() => {}}
-        onToggleOverlay={() => {}}
-      />
-      {
-        // Apparently the only way to extract image data from video elements and image files in Javascript is
-        // to render them to a canvas and extract from there. This strikes me as extremely silly API design,
-        // but I'll just have to deal with it, won't I.
-      }
-      <canvas
-        width={width}
-        height={height}
-        data-testid="test-img-snapshot-extractor"
-      />
-      <canvas
-        width={width}
-        height={height}
-        data-testid="test-img-srcimage-extractor"
-      />
-    </Provider>
-  );
+    return (
+      <Provider theme={defaultTheme}>
+        {
+          track &&
+          <VideoPreview
+            track={track}
+            width={width}
+            height={height}
+            isMainDisplay={true}
+            isOverlay={false}
+            switchesDisabled={false}
+            onToggleMainDisplay={() => {}}
+            onToggleOverlay={() => {}}
+          />
+        }
+        {
+          // Apparently the only way to extract image data from video elements and image files in Javascript is
+          // to render them to a canvas and extract from there. This strikes me as extremely silly API design,
+          // but I'll just have to deal with it, won't I.
+        }
+        <canvas
+          width={width}
+          height={height}
+          data-testid="test-img-snapshot-extractor"
+        />
+        <canvas
+          width={width}
+          height={height}
+          data-testid="test-img-srcimage-extractor"
+        />
+      </Provider>
+    );
+  };
+
+  render(<TestComponent/>);
+
+  // extract image data from source file
+  const srcImageResponse = await fetch(srcImageUrl);
+  const srcImageBlob = await srcImageResponse.blob();
+  const srcImageBitmap = await createImageBitmap(srcImageBlob);
+  const srcImageCanvas = await screen.findByTestId("test-img-srcimage-extractor") as HTMLCanvasElement;
+  const srcImageContext = srcImageCanvas.getContext("2d") as CanvasRenderingContext2D;
+  srcImageContext?.drawImage(srcImageBitmap, 0, 0);
+  const srcImageData = srcImageContext?.getImageData(0, 0, width, height, { colorSpace: "srgb" });
+
+  const stream = srcImageCanvas.captureStream(30);
+  act(() => setPreviewTrack(stream.getVideoTracks()[0]));
 
   const display = await screen.findByRole("img") as HTMLVideoElement;
   await display.play();
@@ -69,15 +79,6 @@ test("VideoPreview displays video track", async () => {
   snapshotContext?.drawImage(display, 0, 0, width, height);
   const snapshotData = snapshotContext?.getImageData(0, 0, width, height, { colorSpace: "srgb" });
 
-  // extract image data from source file
-  const srcImageResponse = await fetch(srcImageUrl);
-  const srcImageBlob = await srcImageResponse.blob();
-  const srcImageBitmap = await createImageBitmap(srcImageBlob);
-  const srcImageCanvas = await screen.findByTestId("test-img-srcimage-extractor") as HTMLCanvasElement;
-  const srcImageContext = srcImageCanvas.getContext("2d") as CanvasRenderingContext2D;
-  srcImageContext?.drawImage(srcImageBitmap, 0, 0);
-  const srcImageData = srcImageContext?.getImageData(0, 0, width, height, { colorSpace: "srgb" });
-
   expect(srcImageData).not.toBeUndefined();
   expect(snapshotData).not.toBeUndefined();
 
@@ -88,33 +89,51 @@ test("VideoPreview displays video track", async () => {
 });
 
 test("VideoPreview handles user input", async () => {
-  const srcImageUrl = "__tests__/assets/lorempicsum-560-640x480.png";
-
-  MediaMock.mock(devices["Mac Desktop"]);
-  await MediaMock.setMediaURL(srcImageUrl);
-
-  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-  const track = stream.getVideoTracks().at(0) as MediaStreamTrack;
-
   const onToggleMainDisplay = vi.fn();
   const onToggleOverlay = vi.fn();
 
   const user = userEvent.setup();
+  let setPreviewTrack: (track: MediaStreamTrack) => void = () => {};
 
-  render(
-    <Provider theme={defaultTheme}>
-      <VideoPreview
-        track={track}
-        width={384}
-        height={216}
-        isMainDisplay={true}
-        isOverlay={false}
-        switchesDisabled={false}
-        onToggleMainDisplay={onToggleMainDisplay}
-        onToggleOverlay={onToggleOverlay}
-      />
-    </Provider>
-  );
+  const TestComponent = () => {
+    const [ track, setTrack ] = useState<MediaStreamTrack>();
+
+    useEffect(() => {
+      setPreviewTrack = setTrack;
+    }, []);
+
+    return (
+      <Provider theme={defaultTheme}>
+        {
+          track &&
+          <VideoPreview
+            track={track}
+            width={384}
+            height={216}
+            isMainDisplay={true}
+            isOverlay={false}
+            switchesDisabled={false}
+            onToggleMainDisplay={onToggleMainDisplay}
+            onToggleOverlay={onToggleOverlay}
+          />
+        }
+        <canvas
+          width={384}
+          height={192}
+          data-testid="track-src"
+        />
+      </Provider>
+    );
+  };
+
+  render(<TestComponent/>);
+
+  const trackSrc = await screen.findByTestId("track-src") as HTMLCanvasElement;
+  const stream = trackSrc.captureStream();
+  const track = stream.getVideoTracks()[0];
+
+  expect(track).not.toBeUndefined();
+  act(() => setPreviewTrack(track));
 
   const mainToggleSwitch = await screen.findByTestId("vp-toggle-main");
   const overlayToggleSwitch = await screen.findByTestId("vp-toggle-overlay");
@@ -127,33 +146,51 @@ test("VideoPreview handles user input", async () => {
 });
 
 test("VideoPreview doesn't handle user input when disabled", async () => {
-  const srcImageUrl = "__tests__/assets/lorempicsum-560-640x480.png";
-
-  MediaMock.mock(devices["Mac Desktop"]);
-  await MediaMock.setMediaURL(srcImageUrl);
-
-  const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-  const track = stream.getVideoTracks().at(0) as MediaStreamTrack;
-
   const onToggleMainDisplay = vi.fn();
   const onToggleOverlay = vi.fn();
 
   const user = userEvent.setup();
+  let setPreviewTrack: (track: MediaStreamTrack) => void = () => {};
 
-  render(
-    <Provider theme={defaultTheme}>
-      <VideoPreview
-        track={track}
-        width={384}
-        height={216}
-        isMainDisplay={true}
-        isOverlay={false}
-        switchesDisabled={true}
-        onToggleMainDisplay={onToggleMainDisplay}
-        onToggleOverlay={onToggleOverlay}
-      />
-    </Provider>
-  );
+  const TestComponent = () => {
+    const [ track, setTrack ] = useState<MediaStreamTrack>();
+
+    useEffect(() => {
+      setPreviewTrack = setTrack;
+    }, []);
+
+    return (
+      <Provider theme={defaultTheme}>
+        {
+          track &&
+          <VideoPreview
+            track={track}
+            width={384}
+            height={216}
+            isMainDisplay={true}
+            isOverlay={false}
+            switchesDisabled={true}
+            onToggleMainDisplay={onToggleMainDisplay}
+            onToggleOverlay={onToggleOverlay}
+          />
+        }
+        <canvas
+          width={384}
+          height={192}
+          data-testid="track-src"
+        />
+      </Provider>
+    );
+  };
+
+  render(<TestComponent/>);
+
+  const trackSrc = await screen.findByTestId("track-src") as HTMLCanvasElement;
+  const stream = trackSrc.captureStream();
+  const track = stream.getVideoTracks()[0];
+
+  expect(track).not.toBeUndefined();
+  act(() => setPreviewTrack(track));
 
   const mainToggleSwitch = await screen.findByTestId("vp-toggle-main");
   const overlayToggleSwitch = await screen.findByTestId("vp-toggle-overlay");
