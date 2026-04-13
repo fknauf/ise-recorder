1353d0db7758983827afcac974394749bf907b41
Florian Knauf <knauf@vss.uni-hannover.de>
2026-04-13 10:33:50 +0200

    make ci for backend leaner

---
+++ tag_images.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+PUSH=no
+eval set -- $(getopt --longoptions push -o '' -- "$@")
+
+while [ $# -gt 0 ]; do
+    case $1 in
+    (--push)
+        shift
+        PUSH=yes
+        ;;
+    (--)
+        shift
+        break
+        ;;
+    esac
+done
+
+DEST_TAG="${1:-v1}"
+SOURCE_TAG="${2:-latest}"
+
+image_names() {
+  yq -r '.services[].image | sub("^"; "echo ")' compose.yml | ISE_RECORD_TAG="$1" bash
+}
+
+while IFS= read -u 3 dest && IFS= read -u 4 src; do
+    docker image tag "$src" "$dest"
+done 3< <(image_names "$DEST_TAG") 4< <(image_names "$SOURCE_TAG")
+
+if [ "$PUSH" == "yes" ]; then
+    for img in $(image_names "$DEST_TAG"); do
+	docker push "$img"
+    done
+fi
