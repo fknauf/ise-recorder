b02d1ba8e0714cf484ff9bc05aabe93b8e5344d0
Florian Knauf <knauf@vss.uni-hannover.de>
2026-09-08 21:56:55 +0200

    Simplified oidc callback route

--- frontend/app/auth/callback/page.tsx
+++ frontend/app/auth/callback/page.tsx
@@ -1,19 +1,7 @@
 "use client";
 
-import { Content, Heading, InlineAlert, ProgressCircle } from "@adobe/react-spectrum";
-import { useAuth } from "react-oidc-context";
+import { AuthStatusMessage } from "@/app/lib/components/AuthStatusMessage";
 
 export default function AuthCallback() {
-  const auth = useAuth();
-
-  if(auth.isLoading) {
-    return <ProgressCircle isIndeterminate size="L"/>;
-  }
-
-  if(auth.error !== undefined) {
-    return <InlineAlert variant="negative">
-      <Heading>AuthenticationFailed</Heading>
-      <Content>{auth.error.message}</Content>
-    </InlineAlert>
-  }
+  return <AuthStatusMessage/>
 }
